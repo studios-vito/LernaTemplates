55ab509b659574322030f05dc1d7620ff1281cb1
Diego <arnes.diego@gmail.com>
2021-10-18 16:58:49 +0800

    test nuxt config

--- README.md
+++ README.md
@@ -5,9 +5,17 @@
 https://lerna.js.org/
 
 https://medium.com/dailyjs/5-tips-for-sharing-code-between-nuxtjs-projects-6ffb5b7f8a25
-```
+https://ichi.pro/es/5-consejos-para-compartir-codigo-entre-proyectos-nuxtjs-123125362559525
 
+https://github.com/kiyopikko/nuxt-monorepo-example
+
+https://qiita.com/kiyopikko/items/969d2067478c4570c550#%E3%81%A9%E3%81%86%E3%81%84%E3%81%86%E9%96%8B%E7%99%BA%E4%BD%93%E9%A8%93%E3%81%8B
+
+https://www.linkedin.com/pulse/things-i-wish-had-known-when-started-javascript-monorepo-gorej/?trk=pulse-article_more-articles_related-content-card
+https://www.linkedin.com/pulse/things-i-have-learned-while-maintaining-javascript-monorepo-gorej/
 ## create new project
 
 > lerna create demo1  master-template --yes
 > lerna bootstrap
+
+
--- packages/demo1/nuxt.config.js
+++ packages/demo1/nuxt.config.js
@@ -1,40 +1,15 @@
 import defaultConfig from 'baseTemplate/nuxt.config'
 
 export default {
-
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'baseTemplate',
-    htmlAttrs: {
-      lang: 'en'
+    ...defaultConfig,
+    head: {
+      title: 'demo1',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'demo1 descp' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
   modules: ['baseTemplate/module']
 }
