911a5ac9436d567815393ce1cd5e22aeaec0386f
Diego <arnes.diego@gmail.com>
2021-10-18 18:54:55 +0800

    test remove strapi call

--- packages/demo2/pages/index.vue
+++ packages/demo2/pages/index.vue
@@ -4,10 +4,6 @@
         <base-button />
         <div>
             <h1>test</h1>
-            <h1>{{ template.title }}</h1>
-            <p>{{ template.description }}</p>
-            <p>{{ template.price }} $</p>
-            <nuxt-img provider="strapi" max-width="50%" :src="template.image.url" />
         </div>
     </div>
 </template>
@@ -15,9 +11,6 @@
 export default ({
     // can chose layout of the baseTemplate or own layout
     layout: 'base_default',
-    async asyncData({ $strapi, route }) {
-        const template = await $strapi.$notes.findOne(route.params.id);
-        return { template };
-    },
+
 })
 </script>
