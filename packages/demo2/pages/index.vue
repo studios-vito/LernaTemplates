e178598259b1664e069f250836ce802d60cfc46f
Diego <arnes.diego@gmail.com>
2021-10-18 17:41:00 +0800

    demo2 test

---
+++ packages/demo2/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>DEMO 2</h1>
+        <base-button />
+    </div>
+</template>
+<script >
+export default ({
+    // can chose layout of the baseTemplate or own layout
+    layout: 'base_default',
+})
+</script>
